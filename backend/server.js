f42c14599c547df6c54931dbe8765889cc3a2961
rashedadwan99 <rashedadwan56789@gmail.com>
2022-12-22 01:21:34 +0300

    preparing to host

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv");
-dotenv.config();
+
+require("dotenv").config();
 const express = require("express");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 require("colors");
